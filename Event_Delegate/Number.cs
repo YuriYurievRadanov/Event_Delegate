e27d6ed620535e2fcdb6e5a6743c72fbd973025b
YAVUZ KILIÇ <yavuz.kilic@koton.com.tr>
2019-10-12 12:19:26 +0300

    Event_Delegate

---
+++ Event_Delegate/Number.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event_Delegate
+{
+    class Number
+    {
+        private PrintHelper _printHelper;
+
+        public Number(int val)
+        {
+            _value = val;
+
+            _printHelper = new PrintHelper();
+            //subscribe to beforePrintEvent event
+            _printHelper.beforePrintEvent += printHelper_beforePrintEvent;
+        }
+        //beforePrintevent handler
+        void printHelper_beforePrintEvent(string message)
+        {
+            Console.WriteLine("BeforePrintEvent fires from {0}", message);
+        }
+
+        private int _value;
+
+        public int Value
+        {
+            get => _value;
+            set => _value = value;
+        }
+
+        public void PrintMoney()
+        {
+            _printHelper.PrintMoney(_value);
+        }
+
+        public void PrintNumber()
+        {
+            _printHelper.PrintNumber(_value);
+        }
+    }
+}
---
+++ Event_Delegate/PrintHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event_Delegate
+{
+    public class PrintHelper
+    {
+        public delegate void BeforePrint(string message);
+        public event BeforePrint beforePrintEvent;
+
+
+        public void PrintNumber(int num)
+        {
+            beforePrintEvent?.Invoke("PrintNumber");
+
+            Console.WriteLine("Number: {0,-12:N0}", num);
+        }
+
+        public void PrintDecimal(int dec)
+        {
+            beforePrintEvent?.Invoke("PrintDecimal");
+
+            Console.WriteLine("Decimal: {0:G}", dec);
+        }
+
+        public void PrintMoney(int money)
+        {
+            beforePrintEvent?.Invoke("PrintMoney");
+
+            Console.WriteLine("Money: {0:C}", money);
+        }
+
+        public void PrintTemperature(int num)
+        {
+            beforePrintEvent?.Invoke("PrintTemerature");
+
+            Console.WriteLine("Temperature: {0,4:N1} F", num);
+        }
+
+        public void PrintHexadecimal(int dec)
+        {
+            beforePrintEvent?.Invoke("PrintHexadecimal");
+
+            Console.WriteLine("Hexadecimal: {0:X}", dec);
+        }
+    }
+}
